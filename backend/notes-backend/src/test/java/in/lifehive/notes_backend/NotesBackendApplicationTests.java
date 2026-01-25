ccd1be784e4f5d1ed8098af0d4a7990052ededb9
Shubh Tandon <shubhtandon802@gmail.com>
2026-01-25 21:56:28 +0530

    Backend basic config added

---
+++ backend/notes-backend/src/test/java/in/lifehive/notes_backend/NotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.lifehive.notes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
