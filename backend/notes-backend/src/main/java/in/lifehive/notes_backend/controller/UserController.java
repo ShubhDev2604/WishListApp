7877ea3da268842abda5dcd8e5e6d9b0887e0ee1
Shubh Tandon <shubhtandon802@gmail.com>
2026-01-26 14:33:23 +0530

    created GET, POST apis for fetching and saving , tested on postman

--- backend/notes-backend/src/main/java/in/lifehive/notes_backend/controller/UserController.java
+++ backend/notes-backend/src/main/java/in/lifehive/notes_backend/controller/UserController.java
@@ -1,4 +1,57 @@
 package in.lifehive.notes_backend.controller;
 
+import in.lifehive.notes_backend.model.Users;
+import in.lifehive.notes_backend.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@RestController
+@RequestMapping("/api/notes")
 public class UserController {
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    UserService service;
+
+    @GetMapping("/users")
+    ResponseEntity<?> getAllUsers() {
+        return new ResponseEntity<>(service.fetchAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    ResponseEntity<?> getAllUsers(@PathVariable Long id) {
+        Users user = service.fetchUserById(id);
+        if(user == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Resource not found");
+        } else {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/csrf-token")
+    ResponseEntity<CsrfToken> getCsrfToken(HttpServletRequest request) {
+        return ResponseEntity.ok().body((CsrfToken) request.getAttribute("_csrf"));
+    }
+
+    @PostMapping("/user")
+    ResponseEntity<String> insertNewUser(@RequestBody Users user) {
+        Users user1 = service.insertNewUsers(user);
+        if(user1 != null) {
+            return ResponseEntity
+                    .ok()
+                    .body("User added");
+        } else {
+            return ResponseEntity
+                    .internalServerError()
+                    .body("Something went wrong");
+        }
+    }
 }
--- backend/notes-backend/src/main/java/in/lifehive/notes_backend/service/UserService.java
+++ backend/notes-backend/src/main/java/in/lifehive/notes_backend/service/UserService.java
@@ -1,4 +1,30 @@
 package in.lifehive.notes_backend.service;
 
+import in.lifehive.notes_backend.model.Users;
+import in.lifehive.notes_backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Users> fetchAllUsers() {
+        return repo.findAll();
+    }
+
+    public Users fetchUserById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Users insertNewUsers(Users user) {
+        return repo.save(user);
+    }
+
 }
